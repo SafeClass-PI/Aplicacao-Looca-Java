2c8b14877e1eddbd36f84a6642415e742890af84
ryanservilha <ryan.servilha@sptech.school>
2025-11-29 20:39:51 -0300

    Redirecionando inserção dos dados para o banco da instancia

--- teste-looca/src/main/java/school/sptech/bd/BancoDados.java
+++ teste-looca/src/main/java/school/sptech/bd/BancoDados.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class BancoDados {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/safeclass";
+    private static final String URL =  "jdbc:mysql://54.204.112.127:3306/safeclass?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USUARIO = "root"; // Usuário do MySQL
     private static final String SENHA = "urubu100"; // Senha do MySQL
 
