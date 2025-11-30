8604a5750db28e42f9b00c01eadcac38e6421199
ryanservilha <ryan.servilha@sptech.school>
2025-11-29 21:01:48 -0300

    Anexando melhoria para start na instancia

--- teste-looca/src/main/java/school/sptech/bd/BancoDados.java
+++ teste-looca/src/main/java/school/sptech/bd/BancoDados.java
@@ -6,11 +6,18 @@ import java.sql.SQLException;
 
 public class BancoDados {
 
-    private static final String URL =  "jdbc:mysql://54.204.112.127:3306/safeclass?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USUARIO = "root"; // Usuário do MySQL
-    private static final String SENHA = "urubu100"; // Senha do MySQL
+    private static final String URL = "jdbc:mysql://54.204.112.127:3306/safeclass?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "urubu100";
 
     public Connection conectar() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL não encontrado no JAR!", e);
+        }
+
         return DriverManager.getConnection(URL, USUARIO, SENHA);
     }
 }
